666340c2924d47bb78f604256b687d9f56ca31ee
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-04-15 09:47:41 +0300

    - added checking data in create and update - added creating user in update and delete - moved schemas

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,6 +1,6 @@
 import requests
 from jsonschema import validate
-from tests.schemas import get_single_user, create_user, update_user
+from schemas.schemas import get_single_user, create_user, update_user
 
 url = "https://reqres.in/api/users/"
 user_id = "2"
@@ -18,18 +18,31 @@ def test_post_create_new_user():
     response = requests.post(url, data)
     assert response.status_code == 201
     validate(response.json(), schema=create_user)
+    assert response.json()["name"] == "Neo"
+    assert response.json()["job"] == "chosen"
+
 
 
 def test_put_update_user():
-    data = {"name": "Neo", "job": "chosen"}
-    response = requests.put(url + user_id, data)
+    old_data = {"name": "Neo", "job": "chosen"}
+    response = requests.post(url, old_data)
+    user_to_update = response.json()["id"]
+
+    new_data = {"name": "Morpheus", "job": "leader"}
+    response = requests.put(url + user_to_update, new_data)
     assert response.status_code == 200
     validate(response.json(), schema=update_user)
+    assert response.json()["name"] == "Morpheus"
+    assert response.json()["job"] == "leader"
 
 
 def test_delete_user():
-    response = requests.delete(url + user_id)
-    assert response.status_code == 204
+    data = {"name": "Neo", "job": "chosen"}
+    create_response = requests.post(url, data)
+    user_to_delete = create_response.json()["id"]
+
+    delete_response = requests.delete(url + user_to_delete)
+    assert delete_response.status_code == 204
 
 
 def test_get_user_not_found():
